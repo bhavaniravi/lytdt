1cb519d317df33678a863708bedc67e0d315c5ae
bhavaniravi <bhava0895@gmail.com>
2020-08-02 18:57:43 +0530

    basic functionality of lytdt without exception handling

--- main.go
+++ main.go
@@ -2,41 +2,93 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
-	"github.com/joho/godotenv"
+	"time"
+
 	"github.com/dghubble/go-twitter/twitter"
-    "github.com/dghubble/oauth1"
+	"github.com/dghubble/oauth1"
+	"github.com/joho/godotenv"
 )
 
-func setup_env(){
+var filePath string = "/tmp/lytdt.txt"
+
+// How to load .env variables
+func setupEnv() {
 	godotenv.Load()
 }
 
-func get_env_value(key string) string{
+// os.environ equivalent in python
+func getEnvValue(key string) string {
 	return os.Getenv(key)
 }
 
-func main() {
-	setup_env()
-	CONSUMER_KEY := get_env_value("CONSUMER_KEY")
-	CONSUMER_SECRET := get_env_value("CONSUMER_SECRET")
+// how to read data from a file
+func readFromFile() string {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return ""
+	}
+	return string(data)
+}
 
-	ACCESS_TOKEN := get_env_value("ACCESS_TOKEN")
-	ACCESS_TOKEN_SECRET := get_env_value("ACCESS_TOKEN_SECRET")
-
-	config := oauth1.NewConfig(CONSUMER_KEY, CONSUMER_SECRET)
-    token := oauth1.NewToken(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-    httpClient := config.Client(oauth1.NoContext, token)
-	client := twitter.NewClient(httpClient)
-	
-	search, _, _ := client.Search.Tweets(&twitter.SearchTweetParams{
-		Query: "from:@geeky_bhavani",
-	})
-	
-	for  i, s := range search.Statuses {
-		fmt.Println(i, s.IDStr, s.Text)
+// how to write data to a file
+// how to convert string to byte
+func wrtieFile(data string) {
+	strData := []byte(data)
+	err := ioutil.WriteFile(filePath, strData, 0644)
+	if err != nil {
+		fmt.Println(err)
 	}
 
+}
 
+func getTweets(username string) {
+	consumerKey := getEnvValue("CONSUMER_KEY")
+	consumerSecret := getEnvValue("CONSUMER_SECRET")
+
+	accessToken := getEnvValue("ACCESS_TOKEN")
+	accessTokenSecret := getEnvValue("ACCESS_TOKEN_SECRET")
+
+	// How to use go twitter library
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessTokenSecret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	// Date formatting YeeYaah!
+	fromDate := time.Now().AddDate(-1, 0, -1).Format("20060102")
+	toDate := time.Now().AddDate(-1, 0, 0).Format("20060102")
+	fromDate = fmt.Sprintf("%s0000", fromDate)
+	toDate = fmt.Sprintf("%s2359", toDate)
+
+	// Using twitter search SDK
+	search, _, _ := client.PremiumSearch.SearchFullArchive(&twitter.PremiumSearchTweetParams{
+		Query: fmt.Sprintf("from:%s", username),
+		// Starting  12 am to 12 pm
+		FromDate:   fromDate,
+		ToDate:     toDate,
+		MaxResults: 100,
+	}, "searchText")
+
+	// Looping through and ignoring favcount
+	for _, s := range search.Results {
+		if !s.Retweeted && s.FavoriteCount > 10 {
+			fmt.Printf("https://twitter.com/%s/status/%s\n", username, s.IDStr)
+		}
+	}
+}
+
+func main() {
+	// Hey switch, welcome back after 4 years of Python
+	switch command := os.Args[1]; command {
+	case "add":
+		wrtieFile(os.Args[2])
+	case "list":
+		setupEnv()
+		username := readFromFile()
+		getTweets(username)
+	}
 }
