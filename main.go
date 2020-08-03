9c9272b35bf8c990210afae4d161dd2ac9d71c3a
bhavaniravi <bhava0895@gmail.com>
2020-08-03 23:34:39 +0530

    add premium search

--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
-	"github.com/joho/godotenv"
+
 	"github.com/dghubble/go-twitter/twitter"
-    "github.com/dghubble/oauth1"
+	"github.com/dghubble/oauth1"
+	"github.com/joho/godotenv"
 )
 
-func setup_env(){
+func setup_env() {
 	godotenv.Load()
 }
 
-func get_env_value(key string) string{
+func get_env_value(key string) string {
 	return os.Getenv(key)
 }
 
@@ -25,18 +26,22 @@ func main() {
 	ACCESS_TOKEN_SECRET := get_env_value("ACCESS_TOKEN_SECRET")
 
 	config := oauth1.NewConfig(CONSUMER_KEY, CONSUMER_SECRET)
-    token := oauth1.NewToken(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+	token := oauth1.NewToken(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-    httpClient := config.Client(oauth1.NoContext, token)
+	httpClient := config.Client(oauth1.NoContext, token)
 	client := twitter.NewClient(httpClient)
-	
-	search, _, _ := client.Search.Tweets(&twitter.SearchTweetParams{
-		Query: "from:@geeky_bhavani",
-	})
-	
-	for  i, s := range search.Statuses {
-		fmt.Println(i, s.IDStr, s.Text)
-	}
+	username := "geeky_bhavani"
+	search, _, _ := client.PremiumSearch.SearchFullArchive(&twitter.PremiumSearchTweetParams{
+		Query:      fmt.Sprintf("from:%s", username),
+		FromDate:   "201907010000",
+		ToDate:     "201907012359",
+		MaxResults: 10,
+	}, "searchText")
 
+	for _, s := range search.Results {
+		if !s.Retweeted && s.FavoriteCount > 5 {
+			fmt.Printf("https://twitter.com/%s/status/%s\n", username, s.IDStr)
+		}
+	}
 
 }
